103dd7be0be32acee0767b06f0fa30422ff4bcce
Travis Roberts <27257343+tsrob50@users.noreply.github.com>
2018-05-12 17:07:14 -0500

    Update Template.ps1
    add logging to event log

--- Template.ps1
+++ Template.ps1
@@ -32,8 +32,8 @@ Param (
 #Script Version
 $sScriptVersion = '1.0'
 
-#Set Error Action to stop
-$ErrorActionPreference = 'Stop'
+# Set Error Action to stop - Best set for each command
+# $ErrorActionPreference = 'Stop'
 
 
 #Log File Info
@@ -41,12 +41,25 @@ $dir = (get-location).ToString() + '\'
 $logFile = $dir + 'Log\' + (get-date -format 'yyyyMMdd') +'_OutputLog.txt'
 Try {
     if ((Get-ChildItem -Attributes 'directory').name -notcontains 'Log') {
-        New-Item -Type Directory .\Log
+        New-Item -ErrorAction stop -Type Directory .\Log
     }
 }
 Catch {
     Write-Host "Error creating log file directory $logfile" -ForegroundColor Red
     Break
+}
+
+# EventLog Info
+# Set the event log to write events to, the source, event ID and source
+# will use the script name unless source is set otherwise
+$eventLog = "Application"
+$eventSource = (split-path $MyInvocation.MyCommand.Definition -Leaf)
+$eventID = 4000
+$entryType = "Error"
+
+# Verify that the Event Log Source exists or creates it
+If ([System.Diagnostics.EventLog]::SourceExists($eventSource) -eq $False) {
+    New-EventLog -LogName $eventLog -Source $eventSource
 }
 
 
@@ -59,16 +72,23 @@ function write-log {
     Write-output "$(get-date -format 'MM/dd/yy HH:mm:ss') $message" | Out-File $logFile -Append
 }
 
+# Event Log Logging
+function write-appEventLog {
+    Param($errorMessage)
+    Write-EventLog -LogName $eventLog -EventID $eventID -EntryType $entryType -Source $eventSource -Message $errorMessage 
+}
+
+
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
 
 
 # Gather User Information
 Try {
-    write-log ('Script ran by ' + ($env:UserDomain + '\' + $env:username) + ' from ' + $env:COMPUTERNAME)
+    write-log -ErrorAction stop ('Script ran by ' + ($env:UserDomain + '\' + $env:username) + ' from ' + $env:COMPUTERNAME)
 }
 Catch {
     $ErrorMessage = $_.Exception.message
-    write-log 'Error gathering user information ' + $ErrorMessage)
+    write-log ('Error gathering user information ' + $ErrorMessage)
     Break
 }
 
